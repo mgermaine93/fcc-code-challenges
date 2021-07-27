82f8c26814228474ae2a2f40f4b4d1da157d4ad3
mgermaine93 <mgermaine93@gmail.com>
2021-07-27 13:51:17 -0400

    Working on putting together the x-axis and labels.

--- scientific-computing-with-python/budget-app/test.py
+++ scientific-computing-with-python/budget-app/test.py
@@ -18,22 +18,24 @@
 ##################### Putting the basic graph together ####################
 amounts = [10, 20, 30, 0]
 y_axis = 100
-final_product = []
+final_product = ["Percentage spent by category\n"]
 
 while y_axis >= 0:
     line = []
     if y_axis == 100:
-        line = [f"{y_axis} |"]
+        line = [f"{y_axis}|"]
     elif y_axis == 0:
-        line = [f"  {y_axis} |"]
+        line = [f"  {y_axis}|"]
     else:
-        line = [f" {y_axis} |"]
+        line = [f" {y_axis}|"]
     for i in range(len(amounts)):
         # Need to double-check this another day...
+        # if an "o" is needed, if it's the last category, and we're at the bottom of the graph...
         if amounts[i] >= y_axis and i == (len(amounts) - 1) and y_axis == 0:
-            line.append("o  ")
+            line.append("o  \n")
+        # if an "o" is not needed, if it's the last category, and we're at the bottom of the graph...
         elif amounts[i] <= y_axis and i == (len(amounts) - 1) and y_axis == 0:
-            line.append("   ")
+            line.append("   \n")
         elif amounts[i] >= y_axis and i == (len(amounts) - 1):
             line.append("o  \n")
         elif amounts[i] >= y_axis:
@@ -47,17 +49,53 @@ while y_axis >= 0:
     # print(formatted_line)
     final_product.append(formatted_line)
     y_axis -= 10
-print("".join(final_product))
+x_axis = ["    "]
+x_axis.append((len(amounts)) * "---")
+# I'm sure there's a more pythonic way to add this extra dash...
+x_axis.append("-")
+final_product.append("".join(x_axis))
+# print("".join(final_product)) # this prints out the chart
 ##################### END Putting the basic graph together ####################
 
-# print(final_product)
-#print(" ".join(final_product))
+# Putting the x-asix together
+names = ["Edwin", "Frazier", "and", "Jane", "Plainwell"]
+longest_name = max(names, key=len)
+formatted_names = []
+for name in names:
+    formatted_names.append(name.ljust(len(longest_name)))
+# print(formatted_names)
 
-test_y_axis = ["100|", " 90| ", " 80| ", " 70| ", " 60| ",
-               " 50| ", " 40| ", " 30| ", " 20| ", " 10| ", "  0| "]
+# loop through the formatted names
+labels = []
+limit = len(longest_name)
+# while limit >= 0:
+# for j in range(len(formatted_names)):
+for name in formatted_names:
+    line = ["    "]
+    for character in name:
+        line.append(f" {character} ")
+    line.append("\n")
+    labels.append(line)
+    # for j in range(len(formatted_names)):
+    #     # Need to iterate through the names themselves, not the list of names!
+    #     line.append(f" {formatted_names[j]} ")
+    # labels.append(line)
+    # limit -= 1
+print(labels)
+# print(labels)
+
+
+# names = [category.name for category in categories]
+#  longest_name = max(names, key=len)
+#   formatted_names = []
+#    for name in names:
+#         formatted_names.append(name.ljust(len(longest_name)))
+#     print(formatted_names)
+
 
 # print("\n".join(test_y_axis))
 
+# Example graph:
 # Percentage spent by category
 # 100|
 #  90|
