aa44d46fd7ad51dc48c106ecf09f9bb49f63f419
mgermaine93 <mgermaine93@gmail.com>
2022-02-07 18:08:01 -0500

    Added functionality to read in CSV data, clean the data, and plot the line chart.

--- data-analysis-with-python/page-view-time-series-visualizer/time_series_visualizer.py
+++ data-analysis-with-python/page-view-time-series-visualizer/time_series_visualizer.py
@@ -1,19 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv(
+    filepath_or_buffer='fcc-forum-pageviews.csv',
+    parse_dates=['date'],
+    index_col='date'
+)
 
 # Clean data
-df = None
+# thanks to https://towardsdatascience.com/10-examples-that-will-make-you-use-pandas-query-function-more-often-a8fb3e9361cb
+low_end = df.value.quantile(0.025)
+high_end = df.value.quantile(0.975)
+cleaned_data = df.query(f"value > {low_end} and value < {high_end}")
+# print(cleaned_data.index)
+# print(cleaned_data['value'])
 
 
 def draw_line_plot():
-    # Draw line plot
-
+    x_values = cleaned_data.index.tolist()
+    y_values = cleaned_data['value'].tolist()
+    # print(x_values)
+    # print(y_values)
+    plt.plot(x_values, y_values)
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.show()
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
