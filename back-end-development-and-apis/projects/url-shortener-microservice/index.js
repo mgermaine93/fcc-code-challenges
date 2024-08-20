63ba1b428e5fa748d62fb7ebf13c6fa2cb7fbb79
mgermaine93 <mgermaine93@gmail.com>
2024-08-20 12:33:37 -0400

    The POST endpoint for the url shortener microservice is working now.

--- back-end-development-and-apis/projects/url-shortener-microservice/index.js
+++ back-end-development-and-apis/projects/url-shortener-microservice/index.js
@@ -1,22 +1,77 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb")
+const dns = require('dns');
+const url = require('url');
 const app = express();
+
+
+MONGO_URL= process.env.MONGO_URL;
+// const { MongoClient } = require('mongodb');
+
+// set up the mongo DB connection
+const client = new MongoClient(MONGO_URL);
+const database = client.db("url-shortener");
+const urls = database.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// middleware
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// define the schema
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number,
+    required: true
+  }
+})
+
+// compile model from schema
+let Url = mongoose.model("Url", urlSchema, "urls");
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// post the data to save it first
+app.post('/api/shorturl', function(req, res) {
+  const userInputUrl = req.body.url;
+  console.log(userInputUrl);
+  const dnslookup = dns.lookup(new URL(userInputUrl).hostname, async (err, address, family) => {
+    console.log(`Here's the address: ${address}`);
+    if (!address) {
+      res.json({
+        error: 'invalid URL' 
+      });
+    } else {
+      const numDocuments = await urls.countDocuments({});
+      console.log(numDocuments);
+      // console.log(numRecords);
+      // valid url, save to database
+      const urlDocument = {
+        original_url: userInputUrl,
+        short_url: numDocuments
+      }
+      const result = await urls.insertOne(urlDocument);
+      res.json({
+        original_url: userInputUrl,
+        short_url: numDocuments
+      });
+    }
+  });
 });
 
 app.listen(port, function() {
