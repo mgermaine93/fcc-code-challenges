82dbfd1514addf0291aae09d7a5f39588d2e24f7
mgermaine93 <mgermaine93@gmail.com>
2024-09-05 17:29:52 -0400

    Added boiletplate code for the metric-imperial converter project.

---
+++ quality-assurance/projects/metric-imperial-converter/routes/api.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+// routes go in here
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+};
