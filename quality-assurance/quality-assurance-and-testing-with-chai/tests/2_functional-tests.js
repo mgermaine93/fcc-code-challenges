9de9fe7ece91d695d619b6fe4be7824fa2f1a289
mgermaine93 <mgermaine93@gmail.com>
2024-09-05 17:30:35 -0400

    Passing all but two tests for the 'quality assurance and testing with chai' module.

---
+++ quality-assurance/quality-assurance-and-testing-with-chai/tests/2_functional-tests.js
@@ -0,0 +1,116 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const server = require('../server');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+suite('Functional Tests', function () {
+  this.timeout(5000);
+  suite('Integration tests with chai-http', function () {
+    // #1
+    test('Test GET /hello with no name', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .get('/hello')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done();
+        });
+    });
+    // #2
+    test('Test GET /hello with your name', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .get('/hello?name=xy_z')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
+          done();
+        });
+    });
+    // #3
+    test('Send {surname: "Colombo"}', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({
+          "surname": "Colombo"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name,  "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
+          done();
+        });
+    });
+    // #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({
+          "surname": "da Verrazzano"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name,  "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
+    });
+  });
+});
+
+const Browser = require('zombie');
+Browser.site = "http://localhost:3000/";
+
+suite('Functional Tests with Zombie.js', function () {
+  this.timeout(5000);
+
+  // instantiate a new instance of the Browser object
+  const browser = new Browser();
+
+  // direct the browser to the '/' route
+  suiteSetup(function(done) {
+    // https://forum.freecodecamp.org/t/solved-qa-testing-with-chai-simulate-actions-using-a-headless-browser/678128/2
+    return browser.visit('/', done());
+  });
+
+  suite('Headless browser', function () {
+    test('should have a working "site" property', function() {
+      assert.isNotNull(browser.site);
+    });
+  });
+
+  suite('"Famous Italian Explorers" form', function () {
+    // #5
+    test('Submit the surname "Colombo" in the HTML form', function (done) {
+      // .fill() returns a promise...
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          // browser.assert.status
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
+    });
+    // #6
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      assert.fail();
+
+      done();
+    });
+  });
+});
