aeec9e36ead38844051af33ee7d1d571e52fda1f
mgermaine93 <mgermaine93@gmail.com>
2024-11-14 17:28:57 -0500

    Making progress on the basic logic of the Translator() class and building out the route.

--- quality-assurance/projects/american-british-translator/components/translator.js
+++ quality-assurance/projects/american-british-translator/components/translator.js
@@ -3,8 +3,47 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+// all logic goes in here
 class Translator {
 
+    constructor(text, locale) {
+        this._text = text;
+        this._locale = locale;
+    }
+
+    // getters
+    get text() {
+        return this._text;
+    }
+    get locale() {
+        return this._locale;
+    }
+
+    // methods
+
+    // need to somehow filter all of the words out of the input string (since characters, whitespace, etc., don't translate)
+    // if translatable words are not surrounded by whitespace, than we don't need to translate them.
+    getWords() {
+        let splitInputText = this._text.split(" ");
+        console.log(splitInputText);
+        // https://www.geeksforgeeks.org/how-to-remove-all-line-breaks-from-a-string-using-javascript/#
+        const splitInputTextWithWhitespaceAndNewlinesRemoved = [] 
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#for...of_statement
+        for (let word of splitInputText) {
+            // https://www.geeksforgeeks.org/how-to-remove-all-line-breaks-from-a-string-using-javascript/
+            let wordWithNewlinesRemoved = word.split(/[\r\n]+/).filter(Boolean).join('');
+            splitInputTextWithWhitespaceAndNewlinesRemoved.push(wordWithNewlinesRemoved);
+        }
+        return splitInputTextWithWhitespaceAndNewlinesRemoved;
+    }
+
+    americanToBritish() {
+        pass
+    }
+
+    britishToEnglish() {
+        pass
+    }
 }
 
 module.exports = Translator;
--- quality-assurance/projects/american-british-translator/routes/api.js
+++ quality-assurance/projects/american-british-translator/routes/api.js
@@ -3,11 +3,19 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+    // complete this route
+    app.route('/api/translate')
+        .post((req, res) => {
+            console.log("In the route")
+            const translator = new Translator(req.body.text, req.body.locale);
+            const inputText = translator.text;
+            const inputLocale = translator.locale;
+            console.log(`In the route: ${translator.getWords(inputText)}`)
+            res.json({
+                text: inputText,
+                translation: translator.getWords(inputText)
+            })
+            // console.log(translator.locale);
+        });
 };
