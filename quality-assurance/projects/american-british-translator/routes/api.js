c74875723f4b8112c75b6aba835109a075ecdcea
mgermaine93 <mgermaine93@gmail.com>
2024-11-22 17:49:15 -0500

    Built out the route and logic a little bit more.  Still lots to do.

--- quality-assurance/projects/american-british-translator/routes/api.js
+++ quality-assurance/projects/american-british-translator/routes/api.js
@@ -12,11 +12,32 @@ module.exports = function (app) {
             const inputText = translator.text;
             const inputLocale = translator.locale;
             const inputWords = translator.getWords(inputText);
-            console.log(`In the route: ${translator.getWords(inputText)}`)
-            res.json({
-                text: inputText,
-                translation: translator.americanToBritish(inputWords)
-            })
-            // console.log(translator.locale);
+            console.log(`In the route: ${translator.getWords(inputText)}`);
+            // do the american-to-british translation stuff in here
+            if (inputLocale == "american-to-british") {
+                console.log({
+                    text: inputText,
+                    locale: inputLocale,
+                    translation: translator.americanToBritish(inputWords)
+                });
+                res.json({
+                    text: inputText,
+                    locale: inputLocale,
+                    translation: translator.americanToBritish(inputWords)
+                });
+            // do the american-to-british-stuff translation in here
+            } else if (inputLocale == "british-to-american") {
+                console.log({
+                    text: inputText,
+                    locale: inputLocale,
+                    translation: translator.americanToBritish(inputWords)
+                });
+                res.json({
+                    text: inputText,
+                    locale: inputLocale,
+                    translation: translator.americanToBritish(inputWords)
+                });
+            }
         });
+
 };
