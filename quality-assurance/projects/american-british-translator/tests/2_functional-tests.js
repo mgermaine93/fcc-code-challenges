9fcbf934f1930829f049941aab27a8e83f37a7f2
mgermaine93 <mgermaine93@gmail.com>
2024-11-22 17:48:37 -0500

    Added boilerplate test to the functional tests.

--- quality-assurance/projects/american-british-translator/tests/2_functional-tests.js
+++ quality-assurance/projects/american-british-translator/tests/2_functional-tests.js
@@ -7,7 +7,22 @@ chai.use(chaiHttp);
 
 let Translator = require('../components/translator.js');
 
+let translator = new Translator();
+
 suite('Functional Tests', () => {
+
+    // example set up from the metric-imperial converter 
+    // test('Convert a valid input such as 10L: GET request to /api/convert.', (done) => {
+    //     chai
+    //         .request(server)
+    //         .keepOpen()
+    //         .get('/api/convert?input=10L')
+    //         .end(function (err, res) {
+    //             assert.equal(res.status, 200);
+    //             assert.equal(res.text, '{"initNum":10,"initUnit":"L","returnNum":2.64172,"returnUnit":"gal","string":"10 liters converts to 2.64172 gallons"}');
+    //             done();
+    //         });
+    // });
 
     // Translation with text and locale fields: POST request to /api/translate
     // Translation with text and invalid locale field: POST request to /api/translate
