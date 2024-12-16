f53087de0d68d54b456baad946e2501af7bd4748
mgermaine93 <mgermaine93@gmail.com>
2024-12-16 17:24:44 -0500

    Passing 2/11 of FCC's tests for the issue tracker.

--- quality-assurance/projects/issue-tracker/routes/api.js
+++ quality-assurance/projects/issue-tracker/routes/api.js
@@ -1,27 +1,146 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb")
+const MONGO_URL= process.env.MONGO_URL;
+// const { MongoClient } = require('mongodb');
+
+// set up the mongo DB connection
+const client = new MongoClient(MONGO_URL);
+const database = client.db("issue-tracker");
+const issues = database.collection("issues");
+
+// define the schema
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+  issue_title: {
+    type: String,
+    required: true
+  },
+  issue_text: {
+    type: String,
+    required: true
+  },
+  created_by: {
+    type: String,
+    required: true
+  },
+  assigned_to: {
+    type: String,
+    required: false
+  },
+  status_text: {
+    type: String,
+    required: false
+  }
+})
+
+// compile model from schema
+let Issue = mongoose.model("Issue", issueSchema, "issues");
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async (req, res) => {
       let project = req.params.project;
-      
+      console.log(req.body);
+      // TBD
     })
     
-    .post(function (req, res){
+    .post(async (req, res) => {
       let project = req.params.project;
-      
+
+      // construct the valid issue
+      const issueDocument = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text
+      }
+
+      // save it to the database
+      const result = await issues.insertOne(new Issue(issueDocument))
+      console.log(result.insertedId)
+
+      const issue = await issues.findOne({_id: result.insertedId})
+      console.log(`Found issue: ${issue}`)
+
+      res.json({
+        _id: result.insertedId,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text,
+        created_on: issue.created_on,
+        updated_on: issue.updated_on,
+        open: issue.open,
+        status_text: issue.status_text
+      })
     })
     
-    .put(function (req, res){
-      let project = req.params.project;
-      
+    .put(async (req, res) => {
+      console.log(req.body);
+
+      // for a "put", the one required field is "_id"
+      const _id = req.body._id;
+
+      if (!_id) {
+        res.json({
+          error: 'missing _id'
+        })
+      } else {
+
+        const title = req.body.issue_title;
+        const text = req.body.issue_text;
+        const created_by = req.body.created_by;
+        const assigned_to = req.body.assigned_to;
+        const status = req.body.status_text;
+
+        if (!title && !text && !created_by && !assigned_to && !status) {
+          res.json({
+            error: 'no update field(s) sent',
+            '_id': _id
+          })
+        }
+        // try {
+        //   const updatedIssue = await issues.updateOne(
+        //     {_id: _id},
+        //     {}
+        //   )
+        // }
+      }
+
     })
     
-    .delete(function (req, res){
-      let project = req.params.project;
+    .delete(async (req, res) => {
       
+      const idToDelete = req.body._id
+
+      if (!idToDelete) {
+        res.json({
+          error: 'missing _id'
+        });
+      } else {
+        try {
+          const result = await issues.deleteOne({_id: idToDelete});
+          if (result) {
+            res.json({
+              result: 'successfully deleted',
+              '_id': idToDelete
+            })
+          }
+        } catch (e) {
+          res.json({
+            error: 'could not delete',
+            '_id': idToDelete
+          })
+        }
+      }
+
     });
     
 };
--- quality-assurance/projects/issue-tracker/server.js
+++ quality-assurance/projects/issue-tracker/server.js
@@ -1,22 +1,20 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
