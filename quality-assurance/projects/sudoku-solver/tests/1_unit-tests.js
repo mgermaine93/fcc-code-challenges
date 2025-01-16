274110dcedf0242d6281b54e40141f11864e9c00
mgermaine93 <mgermaine93@gmail.com>
2025-01-16 17:12:34 -0500

    Added outlines for the unit and functional tests.  To be completed later.

--- quality-assurance/projects/sudoku-solver/tests/1_unit-tests.js
+++ quality-assurance/projects/sudoku-solver/tests/1_unit-tests.js
@@ -6,4 +6,52 @@ let solver;
 
 suite('Unit Tests', () => {
 
+    test("Logic handles a valid puzzle string of 81 characters.", () => {
+        assert.fail();
+    });
+
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or '.').", () => {
+        assert.fail()
+    });
+
+    test("Logic handles a puzzle string that is not 81 characters in length.", () => {
+        assert.fail()
+    });
+
+    test("Logic handles a valid row placement.", () => {
+        assert.fail();
+    });
+
+    test("Logic handles an invalid row placement.", () => {
+        assert.fail()
+    });
+
+    test("Logic handles a valid column placement.", () => {
+        assert.fail()
+    });
+
+    test("Logic handles an invalid column placement.", () => {
+        assert.fail();
+    });
+
+    test("Logic handles a valid region (3x3 grid) placement.", () => {
+        assert.fail()
+    });
+
+    test("Logic handles an invalid region (3x3 grid) placement.", () => {
+        assert.fail()
+    });
+
+    test("Valid puzzle strings pass the solver.", () => {
+        assert.fail();
+    });
+
+    test("Invalid puzzle strings fail the solver.", () => {
+        assert.fail()
+    });
+
+    test("Solver returns the expected solution for an incomplete puzzle.", () => {
+        assert.fail()
+    });
+
 });
--- quality-assurance/projects/sudoku-solver/tests/2_functional-tests.js
+++ quality-assurance/projects/sudoku-solver/tests/2_functional-tests.js
@@ -7,4 +7,80 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
 
+    suite('tests for POST requests to /api/solve', function() {
+
+        test('Solve a puzzle with valid puzzle string: POST request to /api/solve', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Solve a puzzle with missing puzzle string: POST request to /api/solve', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Solve a puzzle with invalid characters: POST request to /api/solve', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Solve a puzzle with incorrect length: POST request to /api/solve', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Solve a puzzle that cannot be solved: POST request to /api/solve', (done) => {
+            assert.fail();
+            done();
+        });
+    });
+
+    suite('tests for POST requests to /api/check', function() {
+
+        test('Check a puzzle placement with all fields: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with single placement conflict: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with multiple placement conflicts: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with all placement conflicts: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with missing required fields: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with invalid characters: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with incorrect length: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with invalid placement coordinate: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+
+        test('Check a puzzle placement with invalid placement value: POST request to /api/check', (done) => {
+            assert.fail();
+            done();
+        });
+    });
+
 });
