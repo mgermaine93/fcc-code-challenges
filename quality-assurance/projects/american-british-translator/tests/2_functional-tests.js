9933b46dabda08d2b84fff5bad12c5fb5327e84d
mgermaine93 <mgermaine93@gmail.com>
2025-02-03 16:28:51 -0500

    Wrote most of the six functional FCC tests for the american-british translator.

--- quality-assurance/projects/american-british-translator/tests/2_functional-tests.js
+++ quality-assurance/projects/american-british-translator/tests/2_functional-tests.js
@@ -11,24 +11,227 @@ let translator = new Translator();
 
 suite('Functional Tests', () => {
 
-    // example set up from the metric-imperial converter 
-    // test('Convert a valid input such as 10L: GET request to /api/convert.', (done) => {
-    //     chai
-    //         .request(server)
-    //         .keepOpen()
-    //         .get('/api/convert?input=10L')
-    //         .end(function (err, res) {
-    //             assert.equal(res.status, 200);
-    //             assert.equal(res.text, '{"initNum":10,"initUnit":"L","returnNum":2.64172,"returnUnit":"gal","string":"10 liters converts to 2.64172 gallons"}');
-    //             done();
-    //         });
-    // });
+    test('Translation with valid text and locale fields: POST request to /api/translate', (done) => {
+        const text = '';
+        const locale = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: text,
+                locale: locale
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(
+                    res.status, 
+                    200
+                );
+                assert.property(
+                    // expected
+                    res.body, 
+                    // actual
+                    'text', 
+                    // message
+                    'the response of a successful POST request with valid "text" and "locale" values should have a property of "text"'
+                );
+                assert.property(
+                    res.body, 
+                    'translation', 
+                    'the response of a successful POST request with valid "text" and "locale" values should have a property of "translation"'
+                );
+                assert.equal(
+                    res.body, 
+                    {
+                        text: text,
+                        translation: translation
+                    },
+                    'the POST request should successfully translate user-input when valid text and locale fields are provided'
+                );
+                done();
+            });
+    });
 
-    // Translation with text and locale fields: POST request to /api/translate
-    // Translation with text and invalid locale field: POST request to /api/translate
-    // Translation with missing text field: POST request to /api/translate
-    // Translation with missing locale field: POST request to /api/translate
-    // Translation with empty text: POST request to /api/translate
-    // Translation with text that needs no translation: POST request to /api/translate
+    test('Translation with valid text and invalid locale field: POST request to /api/translate', (done) => {
+        const text = '';
+        const invalidLocale = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: text,
+                locale: invalidLocale
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(
+                    res.status, 
+                    200
+                );
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with an invalid "locale" value should have a property of "error"'
+                );
+                assert.equal(
+                    res.body, 
+                    { error: 'Invalid value for locale field' },
+                    'the response of a successful POST request with an invalid "locale" value should be { error: "Invalid value for locale field" }'
+                );
+                done();
+            });
+    });
+
+    test('Translation with missing text field: POST request to /api/translate', (done) => {
+        const missingTextField = null;
+        const locale = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: missingTextField,
+                locale: locale
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(
+                    res.status, 
+                    200
+                );
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with an empty "text" value should have a property of "error"'
+                );
+                assert.equal(
+                    res.body, 
+                    { error: 'Required field(s) missing' },
+                    'the response of a successful POST request with an empty "text" value should be { error: "Required field(s) missing" }'
+                );
+                done();
+            });
+    });
+
+    test('Translation with missing locale field: POST request to /api/translate', (done) => {
+        const text = '';
+        const missingLocaleField = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: text,
+                locale: missingLocaleField
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(
+                    res.status, 
+                    200
+                );
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with an empty "locale" value should have a property of "error"'
+                );
+                assert.equal(
+                    res.body, 
+                    { error: 'Invalid value for locale field' },
+                    'the response of a successful POST request with an empty "locale" value should be { error: "Invalid value for locale field" }'
+                );
+                done();
+            });
+    });
+
+    test('Translation with empty text: POST request to /api/translate', (done) => {
+        const emptyTextField = '';
+        const locale = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: emptyTextField,
+                locale: locale
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(res.status, 200);
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with an empty "text" value should have a property of "error"'
+                );
+                assert.equal(
+                    res.body, 
+                    { error: 'No text to translate' },
+                    'the response of a successful POST request with an empty "locale" value should be { error: "No text to translate" }'
+                );
+                done();
+            });
+    });
+
+    test('Translation with text that needs no translation: POST request to /api/translate', (done) => {
+        const allGoodText = '';
+        const locale = '';
+        const translation = '';
+        chai
+            .request(server)
+            .keepOpen()
+            .post('/api/translate')
+            .send({
+                text: allGoodText,
+                locale: locale
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(res.status, 200);
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with text that needs no translation should have a property of "text"'
+                );
+                assert.property(
+                    res.body, 
+                    'error', 
+                    'the response of a successful POST request with text that needs no translation should have a property of "translation"'
+                );
+                assert.equal(
+                    res.body, 
+                    { 
+                        text: text,
+                        translation: "Everything looks good to me!"
+                    },
+                    'the response of a successful POST request with text that needs no translation should have a "translation" value of "Everything looks good to me!"'
+                );
+                done();
+            });
+    });
 
 });
