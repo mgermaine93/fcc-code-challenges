bf4b90a37a16eb4e8b56199f775a99c402e413e0
mgermaine93 <mgermaine93@gmail.com>
2025-03-17 16:59:45 -0400

    Moved all information security projects to their own subdirectory for organization.

--- information-security/port-scanner/port_scanner.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-
-def get_open_ports(target, port_range, verbose=False):
-    open_ports = []
-
-    return open_ports
--- information-security/sha-1-password-cracker/password_cracker.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-
-def crack_sha1_hash(hash, use_salts=False):
-    return True
