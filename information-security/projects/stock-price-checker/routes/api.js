938616013a549be9c687f9ea59491264df65ae84
mgermaine93 <mgermaine93@gmail.com>
2025-03-18 23:15:26 -0400

    Added mongodb and mongoose references.  Almost able to view and like one stock.

--- information-security/projects/stock-price-checker/routes/api.js
+++ information-security/projects/stock-price-checker/routes/api.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-const { MongoClient, ObjectId } = require("mongodb")
+const { MongoClient } = require("mongodb")
 const MONGO_URL= process.env.MONGO_URL;
 
 // set up the mongo DB connection
@@ -45,85 +45,106 @@ module.exports = (app) => {
 
       let baseUrl = "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/";
 
-      const stocks = req.query.stock || '';
+      const stockInput = req.query.stock || '';
 
-      if (!stocks) {
+      if (!stockInput) {
         return res.json({
           error: 'at least one stock symbol is required'
         })
       }
 
-      if (Array.isArray(stocks) && stocks.length == 2) {
+      if (Array.isArray(stockInput) && stockInput.length == 2) {
 
-        // there are two stocks to check
-        const stock1 = req.query.stock[0].trim().toUpperCase() || '';
-        console.log(stock1);
+        return res.json({error: 'there are two stocks to search'})
 
-        const stock2 = req.query.stock[1].trim().toUpperCase() || '';
-        console.log(stock2);
+        // // there are two stocks to check
+        // const stock1 = req.query.stockInput[0].trim().toUpperCase() || '';
+        // console.log(stock1);
+        // const dbStock1 = await stocks.findOne({stock: stock1});
 
-        const like = req.query.like || false;
-        console.log(like);
+        // const stock2 = req.query.stockInput[1].trim().toUpperCase() || '';
+        // console.log(stock2);
+        // const dbStock2 = await stocks.findOne({stock: stock2});
 
-        const stockData1 = await price(`${baseUrl}${stock1}/quote`);
-        console.log(stockData1);
+        // const like = req.query.like || false;
+        // console.log(like);
 
-        const stockData2 = await price(`${baseUrl}${stock1}/quote`);
-        console.log(stockData2);
+        // const stockData1 = await price(`${baseUrl}${stock1}/quote`);
+        // console.log(stockData1);
 
-        if (!stockData1 || !stockData2) {
-          return res.json({error: 'could not retrieve information about at least one of the stocks'});
-        } else {
+        // const stockData2 = await price(`${baseUrl}${stock1}/quote`);
+        // console.log(stockData2);
 
-          const stock1Symbol = stockData1.symbol;
-          const stock1Price = stockData1.latestPrice;
-          const stock1Likes = '' // TBD, need to do a DB for this I think
+        // if (!stockData1 || !stockData2) {
+        //   return res.json({error: 'could not retrieve information about at least one of the stocks'});
+        // } else {
 
-          const stock2Symbol = stockData2.symbol;
-          const stock2Price = stockData2.latestPrice;
-          const stock2Likes = '' // TBD, need to do a DB for this I think
+        //   const stock1Symbol = stockData1.symbol;
+        //   const stock1Price = stockData1.latestPrice;
+        //   const stock1Likes = '' // TBD, need to do a DB for this I think
 
-          return res.json({
-            'stockData': [
-              {
-                'stock': stock1Symbol,
-                'price': stock1Price,
-                'rel_likes': stock1Likes
-              },
-              {
-                'stock': stock2Symbol,
-                'price': stock2Price,
-                'rel_likes': stock2Likes
-              },
-            ]
-          })
-        }
+        //   const stock2Symbol = stockData2.symbol;
+        //   const stock2Price = stockData2.latestPrice;
+        //   const stock2Likes = '' // TBD, need to do a DB for this I think
+
+        //   return res.json({
+        //     'stockData': [
+        //       {
+        //         'stock': stock1Symbol,
+        //         'price': stock1Price,
+        //         'rel_likes': stock1Likes
+        //       },
+        //       {
+        //         'stock': stock2Symbol,
+        //         'price': stock2Price,
+        //         'rel_likes': stock2Likes
+        //       },
+        //     ]
+        //   })
+        // }
 
       }
 
-      else if (typeof stocks === "string") {
+      else if (typeof stockInput === "string") {
 
         // there is one stock to check
         const stock = req.query.stock.trim().toUpperCase() || '';
-        console.log(stock);
-      
         const like = req.query.like || false;
-        console.log(like);
 
         const stockData = await price(`${baseUrl}${stock}/quote`);
+        
         if (!stockData) {
           return res.json({error: 'could not retrieve information about the stock'});
         } else {
+
           const stockSymbol = stockData.symbol;
           const stockPrice = stockData.latestPrice;
-          const likes = '' // TBD, need to do a DB for this I think
-          return res.json({
-            'stockData': {
-              'stock': stockSymbol,
-              'price': stockPrice,
-              'likes': likes
+          // const likes = '' // TBD, need to do a DB for this I think
+          const stock = await stocks.findOne({stock: stockSymbol});
+          
+          if (!stock) {
+            // create the stock in the database
+            const newStock = new Stock({
+              stock: stockSymbol,
+              price: stockPrice,
+              likes: like ? 1 : 0 // might need to fix this
+            })
+              
+            const savedStock = await stocks.insertOne(newStock);
+            if (!savedStock) {
+              return res.json({error: 'could not save the stock to the database'});
+            } else {
+              const stockToReturn = await stocks.findOne({stock: stockSymbol});
+              return res.json({
+                'stockData': {
+                  stock: stockToReturn.stock,
+                  price: stockToReturn.price,
+                  likes: stockToReturn.likes
+                }
+              })
             }
-          })
+          }
+
         }
       }
 
