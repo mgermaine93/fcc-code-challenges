91cb845c7cb33a3affb8caf7c60283d9e035886a
mgermaine93 <mgermaine93@gmail.com>
2025-03-20 14:49:27 -0400

    Finished writing functional tests.

--- information-security/projects/stock-price-checker/tests/2_functional-tests.js
+++ information-security/projects/stock-price-checker/tests/2_functional-tests.js
@@ -2,6 +2,7 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+let numLikes;
 
 chai.use(chaiHttp);
 
@@ -52,14 +53,34 @@ suite('Functional Tests', () => {
                 assert.property(res.body.stockData, 'likes', 'the response object "stockData" should have a property of "likes"')
                 assert.isNumber(res.body.stockData.likes, 'the "likes" property should be of type number')
                 assert.isAbove(res.body.stockData.likes, 0, 'after liking a stock, the "likes" property should be greater than 0')
+                numLikes = res.body.stockData.likes
                 done();
             });
     });
 
     test('Viewing the same stock and liking it again: `GET` request to `/api/stock-prices/`', (done) => {
-        // TBD
-        assert.fail();
-        done();
+        const stock = "duol";
+        chai
+            .request(server)
+            .keepOpen()
+            .get(`/api/stock-prices?stock=${stock}&like=true`)
+            .end(function (err, res) {
+                if (err) {
+                    console.log(`There was an error: ${err}`);
+                    res.done(err);
+                }
+                assert.equal(res.status, 200);
+                assert.isObject(res.body, 'the response should be an object')
+                assert.property(res.body, 'stockData', 'the response object should have a property of "stockData"')
+                assert.property(res.body.stockData, 'stock', 'the response object "stockData" should have a property of "stock"')
+                assert.isString(res.body.stockData.stock, 'the "stock" property should be of type string')
+                assert.property(res.body.stockData, 'price', 'the response object "stockData" should have a property of "price"')
+                assert.isNumber(res.body.stockData.price, 'the "price" property should be of type number')
+                assert.property(res.body.stockData, 'likes', 'the response object "stockData" should have a property of "likes"')
+                assert.isNumber(res.body.stockData.likes, 'the "likes" property should be of type number')
+                assert.deepEqual(res.body.stockData.likes, numLikes, 'after trying to like a stock that you have already liked, the "likes" property should remain unchanged')
+                done();
+            });
     });
 
     test('Viewing two stocks: `GET` request to `/api/stock-prices/`', (done) => {
@@ -78,7 +99,7 @@ suite('Functional Tests', () => {
                 assert.isObject(res.body, 'the response should be an object')
                 assert.property(res.body, 'stockData', 'the response object should have a property of "stockData"')
                 assert.isArray(res.body.stockData, 'the response object "stockData" should have a property that is of type array')
-                assert.lengthOf(res.body.stockData, 'the response object "stockData" should have a property that is of type array and has a length of 2')
+                assert.lengthOf(res.body.stockData, 2, 'the response object "stockData" should have a property that is of type array and has a length of 2')
                 assert.property(res.body.stockData[0], 'stock', 'the response object "stockData" should have a property of "stock"')
                 assert.isString(res.body.stockData[0].stock, 'the "stock" property should be of type string')
                 assert.property(res.body.stockData[0], 'price', 'the response object "stockData" should have a property of "price"')
@@ -111,21 +132,21 @@ suite('Functional Tests', () => {
                 assert.isObject(res.body, 'the response should be an object')
                 assert.property(res.body, 'stockData', 'the response object should have a property of "stockData"')
                 assert.isArray(res.body.stockData, 'the response object "stockData" should have a property that is of type array')
-                assert.lengthOf(res.body.stockData, 'the response object "stockData" should have a property that is of type array and has a length of 2')
+                assert.lengthOf(res.body.stockData, 2, 'the response object "stockData" should have a property that is of type array and has a length of 2')
                 assert.property(res.body.stockData[0], 'stock', 'the response object "stockData" should have a property of "stock"')
                 assert.isString(res.body.stockData[0].stock, 'the "stock" property should be of type string')
                 assert.property(res.body.stockData[0], 'price', 'the response object "stockData" should have a property of "price"')
                 assert.isNumber(res.body.stockData[0].price, 'the "price" property should be of type number')
                 assert.property(res.body.stockData[0], 'rel_likes', 'the response object "stockData" should have a property of "rel_likes"')
                 assert.isNumber(res.body.stockData[0].rel_likes, 'the "rel_likes" property should be of type number')
-                assert.isAbove(res.body.stockData[0].rel_likes, 0, 'after liking a stock, the "rel_likes" property should be greater than 0')
+                // assert.isAbove(res.body.stockData[0].rel_likes, 0, 'after liking a stock, the "rel_likes" property should be greater than 0')
                 assert.property(res.body.stockData[1], 'stock', 'the response object "stockData" should have a property of "stock"')
                 assert.isString(res.body.stockData[1].stock, 'the "stock" property should be of type string')
                 assert.property(res.body.stockData[1], 'price', 'the response object "stockData" should have a property of "price"')
                 assert.isNumber(res.body.stockData[1].price, 'the "price" property should be of type number')
                 assert.property(res.body.stockData[1], 'rel_likes', 'the response object "stockData" should have a property of "rel_likes"')
                 assert.isNumber(res.body.stockData[1].rel_likes, 'the "rel_likes" property should be of type number')
-                assert.isAbove(res.body.stockData[1].rel_likes, 0, 'after liking a stock, the "rel_likes" property should be greater than 0')
+                // assert.isAbove(res.body.stockData[1].rel_likes, 0, 'after liking a stock, the "rel_likes" property should be greater than 0')
                 assert.deepEqual(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0, 'the sum of the rel_likes of both stocks should be 0')
                 done();
             });
