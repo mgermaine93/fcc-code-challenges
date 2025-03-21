f12a3f2452a6dfb53b644dd52f84268f7941399c
mgermaine93 <mgermaine93@gmail.com>
2025-03-21 16:46:05 -0400

    Added functional test blueprints.

--- information-security/projects/anonymous-message-board/tests/2_functional-tests.js
+++ information-security/projects/anonymous-message-board/tests/2_functional-tests.js
@@ -7,4 +7,64 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+    // Creating a new thread: POST request to /api/threads/{board}
+    test('Creating a new thread: POST request to `/api/threads/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
+    test('Viewing the 10 most recent threads with 3 replies each: `GET request to /api/threads/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+    test('Deleting a thread with the incorrect password: DELETE request to `/api/threads/{board} with an invalid delete_password`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+    test('Deleting a thread with the correct password: DELETE request to `/api/threads/{board} with a valid delete_password`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Reporting a thread: PUT request to /api/threads/{board}
+    test('Reporting a thread: PUT request to `/api/threads/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Creating a new reply: POST request to /api/replies/{board}
+    test('Creating a new reply: POST request to `/api/replies/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Viewing a single thread with all replies: GET request to /api/replies/{board}
+    test('Viewing a single thread with all replies: GET request to `/api/replies/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+    test('Deleting a reply with the incorrect password: DELETE request to `/api/replies/{board}` with an invalid delete_password', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+    test('Deleting a reply with the correct password: DELETE request to `/api/replies/{board} with a valid delete_password`', (done) => {
+        assert.fail();
+        done();
+    });
+
+    // Reporting a reply: PUT request to `/api/replies/{board}`
+    test('Reporting a reply: PUT request to `/api/replies/{board}`', (done) => {
+        assert.fail();
+        done();
+    });
+
 });
